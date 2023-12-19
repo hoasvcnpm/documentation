8eec78ab6c41d24b046f106c875a28c21a45c036
Lưu Nghĩa Hòa <hoasvcnpm@gmail.com>
2023-12-19 09:01:32 +0700

    [Update] ads.js

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -5,14 +5,19 @@
 
 	// if(location.hostname.indexOf('photoboxcom.com') == -1) return;
 
-	let script = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5261703613038425" crossorigin="anonymous"></script>';
-	
-	document.querySelector('head').innerHTML += script;
+	let script = document.createElement('script');
+	script.async = 1;
+	script.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5261703613038425";
+	script.setAttribute("crossorigin", "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5261703613038425");
+
+	document.querySelector('head').appendChild(script);
 
 	function scroll(){
 		section.forEach(e => {
 			if(e.getAttribute('data-added') != 1 && window.scrollY + window.innerHeight >= e.offsetTop ) {
 				e.setAttribute('data-added', 1);
+
+				// e.appendChild(script);
 
 				e.innerHTML = html;
 
@@ -39,7 +44,7 @@
 
 		// html = '<img src="https://ps.w.org/cf7-preview/assets/icon-128x128.png?rev=3007867" alt="" />';
 
-		html = script + '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5261703613038425" data-ad-slot="8088219191" data-ad-format="auto"></ins>';
+		html = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-5261703613038425" data-ad-slot="8088219191" data-ad-format="auto"></ins>';
 
 		window.addEventListener('scroll', scroll, false);
 		scroll();
